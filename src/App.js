098273ea860214325449b9755a8fa2a30280036c
Nuria <c.odena.nuria@gmail.com>
2023-01-11 19:36:47 +0100

    adding code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import LightBulb from './components/LightBulb';
+import GetDataFetch from './components/GetDataFetch';
+import GetDataXHR from './components/GetDataXHR';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>The Light Bulb</h1>
-      <LightBulb />
-      
+      <h1>API + FETCH</h1>
+      <GetDataXHR />
+      <GetDataFetch />
     </div>
   );
 }
