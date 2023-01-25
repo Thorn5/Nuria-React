e26d08a9e28c9aece165224b74d1f4b1931dc7d3
Nuria <c.odena.nuria@gmail.com>
2023-01-25 20:51:08 +0100

    adding code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './views/Home';
-import Navbar from './elements/Navbar';
-import IssuesList from './views/Issues';
-import IssueDetail from './views/IssueDetail';
+import React, { useState } from 'react';
+import { AddThoughtForm } from './components/AddThoughtForm';
+import { Thought } from './components/Thought';
+import { generateId, getNewExpirationTime } from './components/utilities';
 
 function App() {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought) => {
+    console.log('adding thought', thought);
+    setThoughts((thoughts) =>[thought, ...thoughts]);
+  }
 
   return (
-    <>
-        <Routes>
-          <Route path='/' element={<Navbar/>}>
-            <Route index element={<Home/>} />
-          </Route>
-          <Route path="/issues" element={<Navbar/>}>
-            <Route index element={<IssuesList />}/>
-            <Route path='issue/:id' element={<IssueDetail />}/>
-          </Route>
-        </Routes>
-    </>
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <main>
+        <AddThoughtForm addThought={addThought} />
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} />
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
-export default App;
+export default App
